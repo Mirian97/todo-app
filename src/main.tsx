18b95795c1fc8fb089b43448cd9b272f46006679
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-14 16:45:44 -0300

    feat: add RouterProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import Router from '@/router'
 import '@/styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 )
