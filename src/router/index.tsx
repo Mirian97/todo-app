d4b529417abe922feefe7b332d201d1b968fdc0c
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-14 16:52:53 -0300

    feat: add Error page in errorElement

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,11 +4,13 @@ import { createBrowserRouter } from 'react-router-dom'
 const Hero = lazy(() => import('@/pages/Hero/Hero'))
 const Login = lazy(() => import('@/pages/Login/Login'))
 const SignUp = lazy(() => import('@/pages/SignUp/SignUp'))
+const Error = lazy(() => import('@/pages/Error/Error'))
 
 const Router = createBrowserRouter([
   {
     path: '/',
-    element: <Hero />
+    element: <Hero />,
+    errorElement: <Error />
   },
   {
     path: '/login',
