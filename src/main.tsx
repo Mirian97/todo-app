293dabb5cef883e4df5a1e2ac8acc9fae130081e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-14 19:15:01 -0300

    feat: call Provider from redux inside main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import Router from '@/router'
+import store from '@/store'
 import '@/styles/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <Provider store={store}>
+      <RouterProvider router={Router} />
+    </Provider>
   </React.StrictMode>
 )
