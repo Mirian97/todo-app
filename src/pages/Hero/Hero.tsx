758e4faa0db242fda544139bf115cca743651fd9
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-14 20:41:11 -0300

    feat: initial language setups

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -1,7 +1,12 @@
+import { systemLocale } from '@/i18n'
 import { FC } from 'react'
 
 const Hero: FC = () => {
-  return <div>Hero Page</div>
+  return (
+    <div>
+      Hero Page <>{systemLocale}</>
+    </div>
+  )
 }
 
 export default Hero
