fb72b6fb83ba22b203348ed2bb2cb60f174c3917
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-20 15:42:07 -0300

    feat: create a error error

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,7 +1,26 @@
 import { FC } from 'react'
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
 
 const Error: FC = () => {
-  return <div>Error</div>
+  const error = useRouteError()
+
+  return (
+    <div className='flex h-screen w-screen flex-col items-center justify-center text-center'>
+      <h1 className='text-secondary-400 mb-3 text-2xl font-bold'>Oops!</h1>
+      {isRouteErrorResponse(error) ? (
+        <div>
+          <p>{error.statusText}</p>
+          {error.data?.message && (
+            <p>
+              <i>{error.data.message}</i>
+            </p>
+          )}
+        </div>
+      ) : (
+        <p>{error?.message || 'Unknown Error'}</p>
+      )}
+    </div>
+  )
 }
 
 export default Error
