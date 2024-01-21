47fefa331523951225a443ca6784aad846991269
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 15:37:44 -0300

    style

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -6,7 +6,7 @@ const Error: FC = () => {
 
   return (
     <div className='flex h-screen w-screen flex-col items-center justify-center text-center'>
-      <h1 className='text-secondary-400 mb-3 text-2xl font-bold'>Oops!</h1>
+      <h1 className='mb-3 text-2xl font-bold text-secondary-400'>Oops!</h1>
       {isRouteErrorResponse(error) ? (
         <div>
           <p>{error.statusText}</p>
