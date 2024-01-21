c7e59929ae2ecd89050b5723a7dc75fa9f6747db
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 15:38:24 -0300

    feat: create loadingPage

---
+++ src/components/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,11 @@
+const LoadingPage = () => {
+  return (
+    <div className='bg-cloud-pattern flex h-screen w-screen flex-col items-center justify-center bg-cover backdrop-blur'>
+      <h4 className='loading-text-shadow text-2xl font-bold text-white sm:text-4xl'>
+        Loading...
+      </h4>
+    </div>
+  )
+}
+
+export default LoadingPage
