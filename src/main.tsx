7b9b80aa99d5bd46e38448e7662e01f64e9634e5
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 15:38:10 -0300

    feat: add suspense in router

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import '@/i18n'
 import '@/index.css'
 import Router from '@/router'
 import store from '@/store'
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
+import LoadingPage from './components/LoadingPage/LoadingPage'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={Router} />
+      <Suspense fallback={<LoadingPage />}>
+        <RouterProvider router={Router} />
+      </Suspense>
     </Provider>
   </React.StrictMode>
 )
