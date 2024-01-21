5f06bff23b6eefa0272294d4b6e6ae2b3f637251
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 14:40:33 -0300

    feat: apply rest props in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,10 +4,12 @@ interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonText: string
 }
 
-const Button: FC<IButton> = ({ buttonText, className }) => {
+const Button: FC<IButton> = (props) => {
+  const { buttonText, className, ...rest } = props
   return (
     <button
       className={`ease group relative z-0 min-h-11 w-full overflow-hidden rounded-full border-2 border-white bg-transparent font-medium text-white transition-[background-color] duration-300 ${className}`}
+      {...rest}
     >
       <span className='ease absolute inset-x-0 bottom-1/2 z-0 h-0 w-full translate-y-1/2 bg-primary-300 transition-[height] duration-300 group-hover:h-[102%]' />
       <span className='relative z-[1]'>{buttonText}</span>
