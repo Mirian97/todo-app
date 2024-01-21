d2afe2aa628716d5d7b5a32c8fd421ea8b34b01e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 14:26:40 -0300

    feat: improve button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,16 @@
-const Button = () => {
+import { ButtonHTMLAttributes, FC } from 'react'
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonText: string
+}
+
+const Button: FC<IButton> = ({ buttonText, className }) => {
   return (
-    <button className='ease group relative z-0 min-h-11 w-full overflow-hidden rounded-full border-2 border-white bg-transparent font-medium text-white transition-[background-color] duration-300 hover:border-primary-300'>
+    <button
+      className={`ease group relative z-0 min-h-11 w-full overflow-hidden rounded-full border-2 border-white bg-transparent font-medium text-white transition-[background-color] duration-300 ${className}`}
+    >
       <span className='ease absolute inset-x-0 bottom-1/2 z-0 h-0 w-full translate-y-1/2 bg-primary-300 transition-[height] duration-300 group-hover:h-[102%]' />
-      <span className='relative z-[1]'>Sign up</span>
+      <span className='relative z-[1]'>{buttonText}</span>
     </button>
   )
 }
