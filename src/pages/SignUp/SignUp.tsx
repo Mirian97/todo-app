323885e8ba79629f5ad9eac35ff8eb036f2710b6
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 17:23:13 -0300

    feat: create header

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,14 @@
+import SparkLogo from '@/assets/svgs/spark-logo.svg?react'
 import { FC } from 'react'
 
 const SignUp: FC = () => {
-  return <div>SignUp</div>
+  return (
+    <div className='relative h-screen w-screen overflow-x-hidden'>
+      <div className='header'>
+        <SparkLogo />
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
