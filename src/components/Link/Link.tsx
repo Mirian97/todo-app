1d1b99efbab6c17848bc8465e1a5e5749dd8ffc8
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-21 14:27:00 -0300

    feat: improve link component

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,7 +1,35 @@
-const Link = () => {
+import { FC, LinkHTMLAttributes } from 'react'
+
+interface ILink extends LinkHTMLAttributes<HTMLLinkElement> {
+  linkText: string
+  color?: 'primary' | 'secondary' | 'tertiary'
+  animation?: 'fromCenter' | 'fromRight' | 'fromLeft'
+}
+
+const colorClasses = {
+  primary: 'text-primary-400',
+  secondary: 'text-secondary-400',
+  tertiary: 'text-white'
+}
+
+const animationClasses = {
+  fromCenter: '',
+  fromRight: 'origin-bottom-left',
+  fromLeft: 'origin-bottom-right'
+}
+
+const Link: FC<ILink> = (props) => {
+  const { linkText, className, color = 'primary', animation = 'fromCenter' } = props
+
   return (
-    <a className='mb-6 mt-10 font-medium leading-6'>
-      <span>Already a member ?</span>
+    <a
+      className={`custom-link
+        ${className}
+        ${colorClasses[color]}
+        after:${animationClasses[animation]}
+      `}
+    >
+      <span>{linkText}</span>
     </a>
   )
 }
