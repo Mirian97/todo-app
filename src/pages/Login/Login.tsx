cbeaf3be44f38fcceb97147a60f66093a9e6dd22
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-26 18:00:14 -0300

    wip in login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,6 +5,12 @@ const Login: FC = () => {
   return (
     <div className='relative my-auto flex h-screen w-screen justify-center overflow-x-hidden'>
       <HeaderWithLogo />
+      <div className='flex flex-col pt-60'>
+        <input
+          placeholder='Username'
+          className='rounded-full border border-solid border-secondary-500 px-3 py-2 placeholder:text-black/40 focus:border-primary-300'
+        />
+      </div>
     </div>
   )
 }
