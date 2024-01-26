194b2248a410ad14407e38e3d37670e4ffb3a777
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-26 14:58:17 -0300

    feat: add header in login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,12 @@
+import HeaderWithLogo from '@/components/HeaderWithLogo/HeaderWithLogo'
 import { FC } from 'react'
 
 const Login: FC = () => {
-  return <div>Login</div>
+  return (
+    <div className='relative my-auto flex h-screen w-screen justify-center overflow-x-hidden'>
+      <HeaderWithLogo />
+    </div>
+  )
 }
 
 export default Login
