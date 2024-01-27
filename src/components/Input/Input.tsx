36b7228a0b86dd88356fc0ea2149c69356031017
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-26 21:32:03 -0300

    fix: add show passowrd button

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,36 @@
-import { FC, InputHTMLAttributes, ReactElement } from 'react'
+import VisibilityIcon from '@/assets/svgs/visibility-icon.svg?react'
+import VisibilityOffIcon from '@/assets/svgs/visibility-off-icon.svg?react'
+import { FC, InputHTMLAttributes, ReactElement, useState } from 'react'
 
 interface IInput extends InputHTMLAttributes<HTMLInputElement> {
   startIcon?: ReactElement
-  endIcon?: ReactElement
 }
 
 const Input: FC<IInput> = (props) => {
-  const { startIcon, endIcon, ...restProps } = props
+  const { startIcon, type, ...restProps } = props
+  const [showPassword, setShowPassword] = useState<boolean>(false)
+  const togglePassword = () => setShowPassword((sp) => !sp)
+
+  const handleInputType = () => {
+    if (type !== 'password') return type
+    return showPassword ? 'text' : 'password'
+  }
+
+  const renderPasswordButton = () => (
+    <button onClick={togglePassword}>
+      {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+    </button>
+  )
+
   return (
     <label className='flex w-full items-center gap-x-5 rounded-full border-2 border-solid border-secondary-500/40 px-3 py-2 focus-within:border-primary-300'>
       {startIcon}
       <input
+        type={handleInputType()}
         className='grow border-0 p-0 placeholder:text-black/40 focus:ring-transparent'
         {...restProps}
       />
-      {endIcon}
+      {type === 'password' && renderPasswordButton()}
     </label>
   )
 }
