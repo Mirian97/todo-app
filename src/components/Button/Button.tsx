1700a9581920672756d7885f12e00ba09b1eba84
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-27 17:55:01 -0300

    feat: add variants solid and outlined to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,16 +2,38 @@ import { ButtonHTMLAttributes, FC } from 'react'
 
 interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   buttonText: string
+  variant?: 'solid' | 'outlined'
+}
+
+const variantClasses = {
+  solid: {
+    button:
+      'border-primary-300 bg-primary-300 text-white hover:text-primary-300',
+    span: 'bg-white'
+  },
+  outlined: {
+    button: 'border-white bg-transparent text-white',
+    span: 'bg-primary-300'
+  }
 }
 
 const Button: FC<IButton> = (props) => {
-  const { buttonText, className, ...rest } = props
+  const { buttonText, className, variant = 'solid', ...rest } = props
   return (
     <button
-      className={`ease group relative z-0 min-h-11 w-full overflow-hidden rounded-full border-2 border-white bg-transparent font-medium text-white transition-[background-color] duration-300 ${className}`}
+      className={`
+        ease group relative z-0 min-h-11 w-full overflow-hidden rounded-full border-2 font-medium transition-[background-color] duration-300
+        ${className}
+        ${variantClasses[variant].button}
+      `}
       {...rest}
     >
-      <span className='ease absolute inset-x-0 bottom-1/2 z-0 h-0 w-full translate-y-1/2 bg-primary-300 transition-[height] duration-300 group-hover:h-[102%]' />
+      <span
+        className={`
+          ease absolute inset-x-0 bottom-1/2 z-0 h-0 w-full translate-y-1/2 transition-[height] duration-300 group-hover:h-[102%]
+          ${variantClasses[variant].span}
+        `}
+      />
       <span className='relative z-[1]'>{buttonText}</span>
     </button>
   )
