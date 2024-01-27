40c2e97204e06b92e65815ff2dedb82c82cdf218
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-27 16:43:05 -0300

    feat: add calls in login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,12 @@
 import SecureIcon from '@/assets/images/secure-icon.png'
 import UserIcon from '@/assets/svgs/user-icon.svg?react'
 import Button from '@/components/Button/Button'
+import Checkbox from '@/components/Checkbox/Checkbox'
 import HeaderWithLogo from '@/components/HeaderWithLogo/HeaderWithLogo'
 import Input from '@/components/Input/Input'
 import Link from '@/components/Link/Link'
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useImmer } from 'use-immer'
 
 const DEFAULT_LOGIN_FORM = {
@@ -20,6 +22,8 @@ interface ILoginForm {
 const Login: FC = () => {
   const [formLogin, inputFormLogin] = useImmer<ILoginForm>(DEFAULT_LOGIN_FORM)
   const { username, password } = formLogin
+  const navigate = useNavigate()
+  const navigateToSignUp = () => navigate('/sign-up')
 
   return (
     <div className='relative my-auto flex h-screen w-screen justify-center overflow-x-hidden'>
@@ -47,18 +51,16 @@ const Login: FC = () => {
           }}
         />
         <div className='flex w-full justify-between px-3 text-secondary-300'>
-          <label className='flex cursor-pointer items-center gap-x-2'>
-            <input
-              type='checkbox'
-              className='rounded-sm border-black/60 text-primary-400 focus:ring-primary-400'
-            />
-            <span>Remerber me</span>
-          </label>
+          <Checkbox label='Remember me' />
           <Link linkText='Forgot Password ?' animation='fromLeft' />
         </div>
         <Button buttonText='Sign In' type='submit' />
         <Link linkText="Don't You Have Account ?" color='secondary' />
-        <Link linkText='Sing Up for Free' color='primary' />
+        <Link
+          linkText='Sing Up for Free'
+          color='primary'
+          onClick={navigateToSignUp}
+        />
       </form>
     </div>
   )
