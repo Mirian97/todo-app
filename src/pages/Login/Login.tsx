f08d1c718f66daddd7456ac46c9756c2949cf1c5
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-27 19:07:05 -0300

    fix: styles in login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -3,7 +3,6 @@ import UserIcon from '@/assets/svgs/user-icon.svg?react'
 import AuxiliaryText from '@/components/AuxiliaryText/AuxiliaryText'
 import Button from '@/components/Button/Button'
 import Checkbox from '@/components/Checkbox/Checkbox'
-import HeaderWithLogo from '@/components/HeaderWithLogo/HeaderWithLogo'
 import Input from '@/components/Input/Input'
 import Link from '@/components/Link/Link'
 import { FC } from 'react'
@@ -28,48 +27,45 @@ const Login: FC = () => {
   const navigateToForgotPassword = () => navigate('/forgot-password')
 
   return (
-    <div className='relative my-auto flex h-screen w-screen justify-center overflow-x-hidden'>
-      <HeaderWithLogo />
-      <form className='flex flex-col items-center gap-y-8 pt-60'>
-        <Input
-          value={username}
-          placeholder='Username'
-          startIcon={<UserIcon />}
-          onChange={(e) => {
-            inputFormLogin((draft) => {
-              draft.username = e.target.value
-            })
-          }}
-        />
-        <Input
-          value={password}
-          placeholder='Password'
-          type='password'
-          startIcon={<img src={SecureIcon} alt='Secure icon' />}
-          onChange={(e) => {
-            inputFormLogin((draft) => {
-              draft.password = e.target.value
-            })
-          }}
-        />
-        <div className='flex w-full justify-between px-3 text-secondary-300'>
-          <Checkbox label='Remember me' />
-          <Link
-            linkText='Forgot Password ?'
-            animation='fromRight'
-            size='sm'
-            onClick={navigateToForgotPassword}
-          />
-        </div>
-        <Button buttonText='Sign In' type='submit' />
-        <AuxiliaryText>Don't You Have Account ?</AuxiliaryText>
+    <form className='flex flex-col items-center gap-y-8'>
+      <Input
+        value={username}
+        placeholder='Username'
+        startIcon={<UserIcon />}
+        onChange={(e) => {
+          inputFormLogin((draft) => {
+            draft.username = e.target.value
+          })
+        }}
+      />
+      <Input
+        value={password}
+        placeholder='Password'
+        type='password'
+        startIcon={<img src={SecureIcon} alt='Secure icon' />}
+        onChange={(e) => {
+          inputFormLogin((draft) => {
+            draft.password = e.target.value
+          })
+        }}
+      />
+      <div className='flex w-full justify-between px-3 text-secondary-300'>
+        <Checkbox label='Remember me' />
         <Link
-          linkText='Sing Up for Free'
-          color='primary'
-          onClick={navigateToSignUp}
+          linkText='Forgot Password ?'
+          animation='fromRight'
+          size='sm'
+          onClick={navigateToForgotPassword}
         />
-      </form>
-    </div>
+      </div>
+      <Button buttonText='Sign In' type='submit' />
+      <AuxiliaryText>Don't You Have Account ?</AuxiliaryText>
+      <Link
+        linkText='Sing Up for Free'
+        color='primary'
+        onClick={navigateToSignUp}
+      />
+    </form>
   )
 }
 
