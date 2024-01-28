5a579d4b1431e6023cdd021d3b37cd11326d9cb3
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-28 09:21:41 -0300

    feat: create welcome page to small screens

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,5 +1,24 @@
+import ManWithToDoList from '@/assets/images/man-with-to-do-list.png'
+import Button from '@/components/Button/Button'
+
 const Welcome = () => {
-  return <div>Welcome</div>
+  return (
+    <div className='flex h-full flex-col items-center pb-11'>
+      <img
+        src={ManWithToDoList}
+        alt='Man with todo list'
+        className='max-w-[300px]'
+      />
+      <h4 className='text-primary-600 mb-4 mt-6 text-base font-extrabold'>
+        Welcome to TASKSPARK+
+      </h4>
+      <h5 className='min-h-[67px] max-w-52 text-left text-sm'>
+        TASKSPARK+ will helps you to stay organized and perform your task much
+        faster
+      </h5>
+      <Button buttonText='Get started' className='mt-auto' />
+    </div>
+  )
 }
 
 export default Welcome
