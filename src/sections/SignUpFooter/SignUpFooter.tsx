8541f461df0ff89370fbd64ca69c0e054b29a0fa
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-29 10:45:07 -0300

    fix: add className to add styles in signUp footer

--- src/sections/SignUpFooter/SignUpFooter.tsx
+++ src/sections/SignUpFooter/SignUpFooter.tsx
@@ -1,13 +1,15 @@
 import AuxiliaryText from '@/components/AuxiliaryText/AuxiliaryText'
 import Link from '@/components/Link/Link'
+import { FC, HTMLAttributes } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const SignUpFooter = () => {
+const SignUpFooter: FC<HTMLAttributes<HTMLDivElement>> = (props) => {
+  const { className } = props
   const navigate = useNavigate()
   const navigateToSignUp = () => navigate('/sign-up')
 
   return (
-    <>
+    <div className={`flex w-full flex-col items-center ${className}`}>
       <AuxiliaryText className='mb-7 mt-12'>
         Don't You Have Account ?
       </AuxiliaryText>
@@ -17,7 +19,7 @@ const SignUpFooter = () => {
         onClick={navigateToSignUp}
         className='pb-11'
       />
-    </>
+    </div>
   )
 }
 
