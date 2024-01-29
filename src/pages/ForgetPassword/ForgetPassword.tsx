cc9018d0655cf8b166d698051582bd246bb97f71
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-29 09:24:27 -0300

    fix: styles in forget password page

--- src/pages/ForgetPassword/ForgetPassword.tsx
+++ src/pages/ForgetPassword/ForgetPassword.tsx
@@ -2,28 +2,32 @@ import EmailIcon from '@/assets/svgs/email-icon.svg?react'
 import LockIcon from '@/assets/svgs/lock-icon.svg?react'
 import Button from '@/components/Button/Button'
 import Input from '@/components/Input/Input'
+import SignUpFooter from '@/sections/SignUpFooter/SignUpFooter'
 import { useState } from 'react'
 
 const ForgotPassword = () => {
   const [email, setEmail] = useState<string>('')
 
   return (
-    <div className='text-center'>
+    <div className='-mt-4 flex flex-col items-center overflow-y-hidden'>
       <LockIcon className='mx-auto' />
-      <h4 className='mt-3 text-xl font-semibold text-black'>Forget Password</h4>
-      <p className='text-xs text-black/50'>
+      <h4 className='mt-3 text-xl font-semibold tracking-wide text-black'>
+        Forget Password
+      </h4>
+      <p className='text-[11px] text-black/50'>
         Enter Valid Mail To Reset Password
       </p>
       <Input
         value={email}
         placeholder='Enter'
         startIcon={<EmailIcon />}
-        className='mb-6 mt-9'
+        className='mb-4 mt-6'
         onChange={(e) => {
           setEmail(e.target.value)
         }}
       />
       <Button buttonText='Next' />
+      <SignUpFooter />
     </div>
   )
 }
