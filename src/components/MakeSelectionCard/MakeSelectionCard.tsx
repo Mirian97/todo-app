988ac409ea67a4f981c44d0bdf852dd8f65a379c
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2024-01-29 10:44:21 -0300

    fix: make selection card

--- src/components/MakeSelectionCard/MakeSelectionCard.tsx
+++ src/components/MakeSelectionCard/MakeSelectionCard.tsx
@@ -1,27 +1,27 @@
-import { FC, ReactNode } from 'react'
+import { ButtonHTMLAttributes, FC, ReactNode } from 'react'
 
-interface IMakeSelection {
+interface IMakeSelection extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon: ReactNode
   viaText: string
   viaData: string
-  className?: string
 }
 
 const MakeSelectionCard: FC<IMakeSelection> = (props) => {
-  const { icon, viaData, viaText, className } = props
+  const { icon, viaData, viaText, className, ...restProps } = props
   return (
-    <div
+    <button
       className={`
           shadow-make-selection flex min-h-[120px] items-center gap-x-2 bg-[#D9D9D9] px-4 py-5
           ${className}
         `}
+      {...restProps}
     >
       <div className='w-14 max-w-full'>{icon}</div>
-      <div className='flex flex-col gap-y-1'>
+      <div className='flex flex-col gap-y-1 text-left'>
         <span className='text-xs text-black/50'>{viaText}</span>
         <span className='text-base font-semibold text-black/70'>{viaData}</span>
       </div>
-    </div>
+    </button>
   )
 }
 
